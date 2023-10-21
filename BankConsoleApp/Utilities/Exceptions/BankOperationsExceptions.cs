c57af2d852c72318cfb3b6e4617d2e9c8f6e4c61
NurlanSafarli <nurlansa468@gmail.com>
2023-10-21 13:35:07 +0400

    #exceptionsAdded

---
+++ BankConsoleApp/Utilities/Exceptions/BankOperationsExceptions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankConsoleApp.Utilities.Exceptions
+{
+    internal class BankOperationsExceptions : Exception
+    {
+        public class InsufficientFundsException : Exception
+        {
+            public InsufficientFundsException() : base("Əziz istifadəçi, hesabınızdakı balans çıxış etmək istədiyiniz məbləğdən kiçikdir.") { }
+        }
+        public class AccountNotFoundException : Exception
+        {
+            public AccountNotFoundException() : base("Daxil etdiyiniz hesab mövcud deyil.") { }
+        }
+        public class InvalidAmountException : Exception
+        {
+            public InvalidAmountException() : base("Daxil etdiyiniz məbləğ sıfırdan kiçik və ya mənasızdır.") { }
+        }
+
+
+
+
+    }
+}
